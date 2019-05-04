2c008f9571fbb2fa19b8b586a583a3a8d5c02218
AerhartOne <AerhartOne@gmail.com>
2019-05-05 05:17:51 +0800

    - Modified .gitignore to exclude .vscode, updated config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config(object):
     DEBUG = False
     TESTING = False
-    CSRF_ENABLED = True
+    CSRF_ENABLED = False
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or os.urandom(32)
 
@@ -24,8 +24,6 @@ class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
     ASSETS_DEBUG = False
-    GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-    GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
 
 class TestingConfig(Config):
     TESTING = True
